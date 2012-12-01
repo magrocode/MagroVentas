a7eb77160efdfd508b68cdec754fc952c88fb3ef
Mario Espinoza <mario@magrocode.com>
2012-12-01 15:00:55 -0300

    agregando menu principal

---
+++ application/controllers/paginas.php
@@ -0,0 +1,13 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Paginas extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->helper('url');
+        $this->load->helper('html');
+
+		$this->load->view('templates/header');
+		$this->load->view('paginas/home');
+	}
+}
---
+++ application/views/templates/header.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="<?= base_url('css/bootstrap.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?= base_url('css/bootstrap-responsive.css'); ?>">
+	<title>MagroVentas</title>
+</head>
+<body style="padding-top: 40px;">
+
+<div class="navbar navbar-fixed-top" style="position: absolute;">
+	<div class="navbar-inner">
+		<div class="container" style="width: auto; padding: 0px 20px;">
+			<a class="brand" href="#">MagroVentas</a>
+			<ul class="nav">
+				<li>
+					<a href="#">Ordenes</a>
+				</li>
+				<li>
+					<a href="#">Clientes</a>
+				</li>				
+				<li>
+					<a href="#">Productos</a>
+				</li>
+				<li>
+					<a href="#">Proveedores</a>
+				</li>
+				<li>
+					<a href="#">Sucursales</a>
+				</li>
+				<li>
+					<a href="#">Empleados</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>

a12ea742f8834e2475062315339911d74a66a6ec
Mario Espinoza <mario@magrocode.com>
2012-12-24 18:10:35 -0300

    nombre usuario en barra

--- application/controllers/paginas.php
+++ application/controllers/paginas.php
@@ -5,13 +5,13 @@ class Paginas extends CI_Controller {
 	function __construct()
     {
     	parent::__construct();
-
-    	//$this->load->helper('url');
-        //$this->load->helper('html');
+    	$this->check_isvalidated();
     }
 
 	public function index()
 	{
+		$data['usuario_nombre'] = $this->session->userdata('nombre');
+		
 		$this->load->view('templates/header');
 		$this->load->view('templates/main_menu', $data);
 		$this->load->view('paginas/home');
@@ -27,6 +27,7 @@ class Paginas extends CI_Controller {
 			show_404();
 		}
 		
+		$data['usuario_nombre'] = $this->session->userdata('nombre');
 		$data['title'] = ucfirst($pagina); // Capitalize the first letter
 		
 		$this->load->view('templates/header', $data);
@@ -34,5 +35,12 @@ class Paginas extends CI_Controller {
 		$this->load->view('paginas/'.$pagina, $data);
 		$this->load->view('templates/footer', $data);
 
+	}
+
+	private function check_isvalidated(){
+		// valida el estado de la sesion
+		if(! $this->session->userdata('validated')){
+			redirect('sesion');
+		}
 	}
 }
